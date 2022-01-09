3c32a6954f33aaf780c61f50378ba82801c9d23d
Natalija Sasic <sasicnatalija4@gmail.com>
2022-01-09 10:05:56 +0100

    Initialization of projects

---
+++ officials/src/main/java/com/immunisation/officials/OfficialsApplication.java
@@ -0,0 +1,13 @@
+package com.immunisation.officials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficialsApplication.class, args);
+	}
+
+}
---
+++ officials/src/test/java/com/immunisation/officials/OfficialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.immunisation.officials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
