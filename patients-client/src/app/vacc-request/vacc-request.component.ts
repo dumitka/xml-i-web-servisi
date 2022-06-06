b0fe942d06315cbe15d0d579ed0d5f4c55a9a2b2
Natalija Šašić <sasicnatalija4@gmail.com>
2022-06-06 21:53:56 +0200

    Minor changes

--- patients-client/src/app/vacc-request/vacc-request.component.ts
+++ patients-client/src/app/vacc-request/vacc-request.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-vacc-request',
@@ -16,49 +17,44 @@ export class VaccRequestComponent implements OnInit {
 
   constructor(private formBulder: FormBuilder, 
     private router: Router,
-    private snackBar: MatSnackBar) { }
-    // <!-- Sifra_interesovanja 578
-    // KORISNIK INFO
-    // Drzavljanstvo --> Republika Srbija
-    // Boraviste --> Republika Srbija
-    // Ime --> Ivana
-    // Prezime --> Ivanović
-    // Pol --> Zenski
-    // Datum_rodjenja --> Thu Nov 11 00:00:00 CET 1999
-    // Licni dokument --> 1111999815055
-    // KONTAKT
-    // Broj_mobilnog --> 063560560
-    // Broj_fixnog --> 021778777
-    // Email --> ivanaa@gmail.com
-    // PODACI O VAKCINACIJI
-    // Opstina_ustanove --> Novi Sad
-    // Tip_vakcine --> SINOPHARM
-    // Davalac_krvi --> true
-    // Datum_interesovanja --> Wed Dec 01 00:00:00 CET 2021 -->
-  ngOnInit(): void {
+    private snackBar: MatSnackBar,
+    private authService: AuthService) { }
 
+  ngOnInit(): void {
+    let currentUser = this.authService.getTokenData().user;
     this.interestForm = this.formBulder.group({
 
       drzavljanstvo:["", [Validators.required]],
       boraviste:["", [Validators.required]],
-      ime:["", [Validators.required]],
-      prezime:["", [Validators.required]],
+      ime:[currentUser.ime, [Validators.required]],
+      prezime:[currentUser.prezime, [Validators.required]],
       pol:["", [Validators.required]],
       datum_rodjenja:["", [Validators.required]],
-      licni_dokument:["", [Validators.required]],
+      licni_dokument:[currentUser.jmbg, [Validators.required]],
       broj_mobilnog:["", [Validators.required]],
       broj_fixnog:["", [Validators.required]],
-      email:["", [Validators.email, Validators.required]],
+      email:[currentUser.username, [Validators.email, Validators.required]],
       opstina_ustanove:["", [Validators.required]],
-      tip_vakcine:["", [Validators.required]],
-      davalac_krvi:["", [Validators.required]],
+      tip_vakcine:[""],
+      davalac_krvi:[""],
     })
 
+
+    
+    // this.interestForm.controls['ime'].setValue(currentUser.ime);
+    // this.interestForm.controls['ime'].disable();
+    // this.interestForm.controls['prezime'].setValue(currentUser.prezime);
+    // this.interestForm.controls['prezime'].disable();
+    // this.interestForm.controls['email'].setValue(currentUser.username);
+    // this.interestForm.controls['email'].disable();
+    // this.interestForm.controls['licni_dokument'].setValue(currentUser.jmbg);
+    // this.interestForm.controls['licni_dokument'].disable();
     //DODATI DATUM INTERESOVANJA i 
     //SIFRU INTERESOVANJA
   }
 
   createInterest() {
+    // getRawValue
     alert("Kao kreirano")
     this.router.navigate(["/dashboard"]);
   }
